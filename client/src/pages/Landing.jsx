700451dfd7e6bd2311cec33f7056737e324a0d75
jkchinnu-525 <95326470+jkchinnu-525@users.noreply.github.com>
2024-08-22 19:21:04 +0530

    Update Landing.jsx

--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -6,11 +6,11 @@ export default function Landing() {
       </h1>
       <p className="mb-4 text-slate-700">
         It is a fully functional website where i have used reactjs for my
-        fornt-end and nodejs for my backend.I have used mongodb as my primary
-        database for storage and Supabase for user authentication.Here i have
-        implemented all the routes that are needed for Task-2 and made use of
+        front-end and nodejs for my backend.I have used mongodb as my primary
+        database for storage and Supabase for user authentication and made use of
         many different libraries such as bcrypt,jsonwebtoken and many more to
-        secure the data.
+        secure the data.I have implemented the weather fetching functionality
+        by making use of weather API.
       </p>
     </div>
   );
